bb7cc6d8885e3ae1b4a99cea6117ced86b9da848
KERRCAM <kerrcameron@hotmail.com>
2021-10-06 17:31:45 +0100

    alphabet print

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	char a;
+
+        for ( a = 'A'; a <= 'Z'; ++a) {
+            System.out.println(a + " ");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
